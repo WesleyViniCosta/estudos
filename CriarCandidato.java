d451d1eab932bf438fbfbd4c69866bb6eca012df
WesleyViniCosta <wesleyvinicius_1998@hotmail.com>
2019-10-30 09:06:13 -0300

    Update CriarCandidato.java

--- CriarCandidato.java
+++ CriarCandidato.java
@@ -6,21 +6,16 @@ public class CriarCandidato {
 
     public static void main(String[] args) {
 
-    	Candidato c = new Candidato("Wesley", 24);
+    	Candidato c = new Candidato("Wesley Costa", 17);
         Scanner scan = new Scanner(System.in);
         
         System.out.print("Digite o número do seu candidato: ");
         int numCan = scan.nextInt();
        
-        if(numCan==24){
-        	System.out.println("\nNome: " + c.getNome());
-        	System.out.println("Número: " + c.getNumero());
-        	System.out.println("Votos: " + c.recebeVotos());
-        }else {
-        	System.out.println("\nNome: " + c.getNome());
-        	System.out.println("Número: " + c.getNumero());
-        	System.out.println("Votos: " + c.getVotos());
+        if(numCan == 17) {
+        	c.recebeVotos();
         }
-        
+
+        System.out.println(c);
     }
 }
